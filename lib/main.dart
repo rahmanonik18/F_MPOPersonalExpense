09807dc0287affe5c59ccec71c01a4b68ae43276
rahman <abduronikrahman@gmail.com>
2021-06-19 01:18:10 +0600

    Flutter Personal Expenses

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ main() {
   runApp(MyApp());
 }
 
+String titleInput;
+String amountInput;
+
+final titleController =  TextEditingController();
+final amountController = TextEditingController();
+
 class MyApp extends StatelessWidget {
   final List<Transaction> transactions =[
     Transaction(
@@ -48,11 +54,42 @@ class MyApp extends StatelessWidget {
                 elevation: 10,
               ),
             ),
-            Card(child: Column(
-              children:<Widget> [
+            Card(
+              elevation: 5,
+              child: Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                children:<Widget> [
+                  TextField(
+                    decoration: InputDecoration(labelText: "Title"),
+                    controller: titleController,
+                    //onChanged: (val) {titleInput = val;},
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "Amount"),
+                    controller: amountController,
+                    //onChanged: (val)=> amountInput = val,
+                  ),
 
-              ],
+                  // ignore: deprecated_member_use
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    // ignore: deprecated_member_use
+                    child: FlatButton(
+                        onPressed: (){
+                          print(titleController.text);
+                          print(amountController.text);
+                        },
+                        child: Text("Add Transaction"),
+                        textColor: Colors.purpleAccent,
+                    ),
+                  )
+
+                ],
             ),
+              ),
             ),
             Column(children: transactions.map((tx) {
               return Card(
